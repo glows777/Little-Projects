ceaf78a8cce5f98c34b62e8ccaf17a7eddf6bb43
glows777 <1914426389@qq.com>
2021-04-22 17:30:05 +0800

    Tracker completed

--- 购物车结算/Tracker.js
+++ 购物车结算/Tracker.js
@@ -6,10 +6,15 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
+//获取先前用户存储在本地的数据（transactions数组），这样就可以读取以前用户的数据，做到真正的记账
 const localStorageTransactions = JSON.parse(
-    localStorage.getItem('transacitions')
+    localStorage.getItem('transactions')
 );
-let transactions = localStorage.getItem('transacitions')!== null ? localStorageTransactions : [];
+
+// 判断上面获取的本地数据是否存在，
+// 存在，就把上面获取的那个数据重新传给transactions数组，
+// 不存在（表示用户第一次使用或者之前清空了），新建一个空数组
+let transactions = localStorage.getItem('transactions')!== null ? localStorageTransactions : [];
 
 function addTransacition(e){
     e.preventDefault();
@@ -23,7 +28,7 @@ function addTransacition(e){
             amount:+amount.value
         };
         transactions.push(transaction);
-        addTransacitionDOM(transaction);
+        addTransactionDOM(transaction);
         updateValues();
         updateLocalStorage();
         text.value='';
@@ -35,6 +40,51 @@ function generateID(){
     return Math.floor(Math.random()*100000000);
 }
 
-function addTransacitionDOM(transaction){
-    
+function addTransactionDOM(transaction){
+    const sign = transaction.amount <0 ?'-' : '+';
+    const item = document.createElement("li");
+    item.classList.add(transaction.amount < 0 ?'minus':'plus');
+    item.innerHTML=`
+    ${transaction.text}<span>${sign}${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn" 
+    onclick="removeTransaction(${transaction.id})">
+    X
+    </button>
+    `;
+    list.appendChild(item);
 }
+
+function updateValues(){
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc,item) =>(acc+=item),0).toFixed(2);
+    const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+    const expense = (amounts
+    .filter(item => item < 0)
+    .reduce((acc,item) => (acc += item),0)*-1)
+    .toFixed(2);
+    balace.innerHTML = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
+function removeTransaction(id){
+    transactions = transactions.filter(transaction => transaction.id!==id);
+    updateLocalStorage();
+    init();
+}
+
+function updateLocalStorage(){
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function init(){
+    list.innerHTML = '';
+    transactions.forEach(addTransactionDOM);
+    updateValues();
+}
+
+init();
+form.addEventListener('submit',addTransacition);
